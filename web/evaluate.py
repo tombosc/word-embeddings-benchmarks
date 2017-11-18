6e01c9773d45c18fff94a7e42e01c94b697dfd60
Tom Bosc <bosc.tom@gmail.com>
2017-11-17 19:14:15 -0500

    Fix

--- web/evaluate.py
+++ web/evaluate.py
@@ -365,11 +365,11 @@ def evaluate_similarity(w, X, y):
     if missing_words > 0:
         logger.warning("Missing {} words. Will replace them with mean vector".format(missing_words))
 
-
     mean_vector = np.mean(w.vectors, axis=0, keepdims=True)
-    A = np.vstack(w.get(word, mean_vector) for word in X[:, 0])
-    B = np.vstack(w.get(word, mean_vector) for word in X[:, 1])
-    scores = np.array([v1.dot(v2.T)/(np.linalg.norm(v1)*np.linalg.norm(v2)) for v1, v2 in zip(A, B)])
+    A = np.asarray([w.get(word, mean_vector) for word in X[:, 0]])
+    B = np.asarray([w.get(word, mean_vector) for word in X[:, 1]])
+    scores = np.array([cosine_similarity(v1, v2) for v1, v2 in zip(A, B)])
+    #scores = np.array([v1.dot(v2.T)/(np.linalg.norm(v1)*np.linalg.norm(v2)) for v1, v2 in zip(A, B)])
     return scipy.stats.spearmanr(scores, y).correlation
 
 
